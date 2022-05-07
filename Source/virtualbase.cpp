45ae89eede67ed6121b036b564edb4050b468eae
Soumyadip Nayak <nayaksoumyadip@gmail.com>
2022-05-07 11:50:14 +0530

    Virtual base

---
+++ Source/virtualbase.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+// Student-> Exam; Student-> Sports; Exam, Sports-> Result
+
+class student{
+    protected:
+        int roll;
+    public:
+        void set_roll(int a){
+            roll=a;
+        }
+        void print_roll(void){
+            cout<<"Your roll no:"<<roll<<endl;
+        }
+
+};
+
+class exam : virtual public student{
+    protected:
+        float math, physics;
+    public:
+        void set_marks(float m, float p){
+            math=m;
+            physics=p;
+        }
+        void print_marks(void){
+            cout<<"Your result is here:"<<endl
+            <<"Math=>"<<math<<endl
+            <<"Physics=>"<<physics<<endl;
+        }
+};
+
+class sports : virtual public student{
+    protected:
+        float score;
+    public:
+        void set_score(float s){
+            score=s;
+        }
+        void print_score(void){
+            cout<<"Score in sports=>"<<score<<endl;
+        }
+};
+
+class result: public exam, public sports{
+    private:
+        float total;
+    public:
+        void display(void){
+            total=math+physics+score;
+            print_roll();
+            print_marks();
+            print_score();
+            cout<<"Your total score is=>"<<total<<endl;
+        }
+};
+
+int main()
+{
+    result harry;
+    harry.set_roll(118);
+    harry.set_marks(56,73.5);
+    harry.set_score(20.1);
+    harry.display();
+    return 0;
+}
